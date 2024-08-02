9f4c2283d70f72da0ced93b9e26c5a59c34de3f8
Much Darmawan Iriansyah Syam <mchdarmawan@gmail.com>
2024-08-02 15:42:09 +0900

    set config and change db schema

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ci/sisfo_akademik_siswa/';
+$config['base_url'] = 'http://localhost/ci/siakad/';
 
 /*
 |--------------------------------------------------------------------------
